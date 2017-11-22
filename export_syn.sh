a0d3ae3b580bbba55a2701c7e371a37e18621b89
bsenevir <33914558+bsenevir@users.noreply.github.com>
2017-11-22 18:25:18 -0500

    Add files via upload

--- export_syn.sh
+++ export_syn.sh
@@ -1,6 +1,4 @@
-write -mapped > $design_root/adc8_v4-gate.v
-write_sdc > $design_root/adc8_v4-gate.sdc
-write_sdf > $design_root/adc8_v4-gate.sdf
+write -mapped > $design_root/i2cSlaveTop_v8_s-gate.v
+write_sdc > $design_root/i2cSlaveTop_v8_s-gate.sdc
+write_sdf > $design_root/i2cSlaveTop_v8_s-gate.sdf
 report area > $design_root/area_original_singleShot.rpt
-set_attribute lp_default_toggle_rate adc8_v4 0.1 #GHz
-report power>$design_root/power.rpt
--- load_modules.sh
+++ load_modules.sh
@@ -1,3 +1,3 @@
-set files {adc8_v4.v}
+set files {i2cSlaveTop_v8_s.v i2cSlave_v2.v serialInterface_v2.v registerInterface_v2.v chipBusController_ns_t6.v counterN.v decoder5to24.v timescale.v i2cSlave_define.v}
 read_hdl $files
-elaborate adc8_v4
+elaborate i2cSlaveTop_v8_s
