5164c01d004a2a558f171a82c0ecd7097d2df79a
bsenevir <33914558+bsenevir@users.noreply.github.com>
2017-11-29 16:36:15 -0500

    Add files via upload

--- export_syn.sh
+++ export_syn.sh
@@ -1,4 +1,4 @@
-write -mapped > $design_root/i2cSlaveTop_v8-gate.v
-write_sdc > $design_root/i2cSlaveTop_v8-gate.sdc
-write_sdf > $design_root/i2cSlaveTop_v8-gate.sdf
+write -mapped > $design_root/chip_v2-gate.v
+write_sdc > $design_root/chip_v2-gate.sdc
+write_sdf > $design_root/chip_v2-gate.sdf
 report area > $design_root/area_original_singleShot.rpt
--- load_modules.sh
+++ load_modules.sh
@@ -1,3 +1,3 @@
-set files {i2cSlaveTop_v8.v i2cSlave_v2.v serialInterface_v2.v registerInterface_v2.v chipBusController_p4.v counterN.v decoder3to5.v mux41_en.v timescale.v i2cSlave_define.v}
+set files {chip_v2.v analogPxArray.v i2cSlaveTop_v8.v i2cSlave_v2.v serialInterface_v2.v registerInterface_v2.v chipBusController_p4.v counterN.v decoder3to5.v mux41_en.v timescale.v i2cSlave_define.v}
 read_hdl $files
-elaborate i2cSlaveTop_v8
+elaborate chip_v2
